92dfc8c61304244c5fdfb2d37f8969254c2c81ac
ShironiDesu <alishkingmu@gmail.com>
2024-12-17 19:46:40 +0500

    create next app

---
+++ src/app/layout.js
@@ -0,0 +1,33 @@
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "@/styles/reset.css";
+import "@/styles/globals.css";
+import "@/styles/general.scss";
+import localFont from "next/font/local";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta",
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+const euclidCircularB = localFont({
+  src: "./fonts/Euclid Circular B Regular.ttf",
+  variable: "--euclid-circular",
+  weight: "100 900",
+});
+export const metadata = {
+  title: "AI Consult",
+  description:
+    "Оказание поддержки в получении кредитных продуктов для юридических лиц",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${plusJakartaSans.variable} ${euclidCircularB.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
