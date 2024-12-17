c56c6f83e31635c3bc905380f981a6c2ecb4da3f
ShironiDesu <alishkingmu@gmail.com>
2024-12-18 02:24:29 +0500

    home page done

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -2,7 +2,7 @@ import HomePageSection from "./sections/HomePageSection";
 
 export default function Home() {
   return (
-    <main className="homepage__main">
+    <main className="homepage__main" style={{ paddingTop: "150px" }}>
       <HomePageSection />
     </main>
   );
--- src/app/home/sections/HomePageSection.jsx
+++ src/app/home/sections/HomePageSection.jsx
@@ -3,24 +3,70 @@ import React from "react";
 import HomeCard from "../components/home-card/HomeCard";
 import "./HomePageSections.scss";
 export default function HomePageSection() {
-  const homeData = [{ img: "", graytext: "" }];
+  const homeData = [
+    {
+      img: "/images/HomeIcon1.svg",
+      whiteText: "Налоговый консалтинг",
+      grayText:
+        "Оказание поддержки в получении кредитных продуктов для юридических лиц",
+      classname: "consulting-card",
+    },
+    {
+      img: "/images/HomeIcon2.svg",
+      whiteText:
+        "Оказание поддержки в получении банковских гарантии для юридических лиц",
+      grayText:
+        "Оказание поддержки в получении кредитных продуктов для юридических лиц",
+      classname: "guarantee-card",
+    },
+    {
+      img: "/images/HomeIcon3.svg",
+      whiteText: "Оказание поддержки в получении страховки для юридических лиц",
+      grayText:
+        "Оказание поддержки в получении кредитных продуктов для юридических лиц",
+      classname: "insurance-card",
+    },
+    {
+      img: "/images/HomeIcon4.svg",
+      whiteText:
+        "Оказание поддержки в получении кредитных продуктов для юридических лиц",
+      grayText: "Оказание поддержки в получении кредитных продуктов",
+      classname: "credit-card",
+    },
+    {
+      img: "/images/HomeIcon5.svg",
+      whiteText: "Юридический консалтинг",
+      grayText:
+        "Оказание поддержки в получении кредитных продуктов для юридических лиц",
+      classname: "legal-card",
+    },
+    {
+      img: "/images/HomeIcon6.svg",
+      whiteText: "Бухгалтерский консалтинг",
+      grayText:
+        "Оказание поддержки в получении кредитных продуктов для юридических лиц",
+      classname: "accounting-card",
+    },
+  ];
+
   return (
     <div className="homepage__section">
-      <HomeCard />
       <h1>
         Оказание поддержки в получении кредитных продуктов для юридических лиц
       </h1>
-      <HomeCard />
-
-      <HomeCard />
-
       <h5>
         AI Consult ГИД — Кредитование и Банковские
         <br /> гарантии для юридических лиц
       </h5>
-      <HomeCard />
-      <HomeCard />
-      <HomeCard />
+      {homeData.map((data, index) => (
+        <HomeCard
+          key={index}
+          img={data.img}
+          whiteText={data.whiteText}
+          grayText={data.grayText}
+          classname={data.classname}
+        />
+      ))}
     </div>
   );
 }
